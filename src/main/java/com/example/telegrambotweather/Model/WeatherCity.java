06de815e71e6886f0d4b72c426e6b3ea2da4878a
Dmytro Kononchuk <dimakononchuk12@gmail.com>
2024-06-01 22:49:28 +0300

    weather city change time to humidity

--- src/main/java/com/example/telegrambotweather/Model/WeatherCity.java
+++ src/main/java/com/example/telegrambotweather/Model/WeatherCity.java
@@ -20,7 +20,7 @@ public class WeatherCity {
     private String City;
     private String Country;
 
-    private String time;
+    private int humidity;
     private String utc;
 
     private int temperature;
@@ -32,23 +32,29 @@ public class WeatherCity {
 
     private final DateTimeFormatter DateTimeformatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     private final DateTimeFormatter Timeformatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
     public WeatherCity(JsonNode json){
         City=json.path("name").asText();
         Country=new Locale("",json.path("sys").path("country").asText()).getDisplayCountry(Locale.ENGLISH);
         ZoneOffset zoneOffset = ZoneOffset.ofTotalSeconds(json.path("timezone").asInt());
         temperature=json.path("main").path("temp").asInt();
+        humidity=json.path("main").path("humidity").asInt();
         weatherDescription=json.path("weather").get(0).path("description").asText();
-        time = LocalDateTime.ofInstant(Instant.ofEpochSecond(json.path("dt").asInt()), ZoneId.of("UTC")).format(DateTimeformatter);
-        sunset = LocalDateTime.ofInstant(Instant.ofEpochSecond(json.path("sys").path("sunset").asInt()), ZoneId.of("UTC")).format(Timeformatter);
-        sunrise = LocalDateTime.ofInstant(Instant.ofEpochSecond(json.path("sys").path("sunrise").asInt()), ZoneId.of("UTC")).format(Timeformatter);
+
+        // Отримати зрушення у секундах від UTC
+        int timeOffset=json.path("timezone").asInt()/3600;
+
+        System.out.println(timeOffset);
+        sunset = LocalDateTime.ofInstant(Instant.ofEpochSecond(json.path("sys").path("sunset").asInt()), ZoneId.of(String.format("UTC+%s",timeOffset))).format(Timeformatter);
+        sunrise = LocalDateTime.ofInstant(Instant.ofEpochSecond(json.path("sys").path("sunrise").asInt()), ZoneId.of(String.format("UTC+%s",timeOffset))).format(Timeformatter);
 
     }
     @Override
     public String toString(){
         return "City: "+City+";\n"+
                 "Country: "+Country+";\n"+
-                "Time: "+time+";\n"+
                 "Temperature: "+temperature+"°C;\n"+
+                "Humidity: "+humidity+"%;\n"+
                 "Description: "+weatherDescription+";\n"+
                 "Sunset: "+sunset+";\n"+
                 "Sunrise: "+sunrise+";\n";
